a26a586ea1937984f46be1a17d59cff0492af3fc
Kevin Hurdt <93124068+khurdt@users.noreply.github.com>
2022-08-14 17:18:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -37,12 +37,6 @@ app.use(methodOverride());
 
 app.use(morgan('common'));
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
