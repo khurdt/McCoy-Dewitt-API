0b01cdc10f0fb694e318b58bf1657439ac802bac
Kevin Hurdt <93124068+khurdt@users.noreply.github.com>
2022-08-16 23:39:48 -0500

    updated

--- index.js
+++ index.js
@@ -17,13 +17,6 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-// const corsOptions = {
-//     origin: 'http://localhost:3000',
-//     credentials: true,            //access-control-allow-credentials:true
-//     optionSuccessStatus: 200
-// }
-// app.use(cors(corsOptions));
-
 let allowedOrigins = [
     'https://kh-movie-app.herokuapp.com',
     'http://127.0.0.1:8080',
