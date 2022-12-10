289759acd85e297a2114305986a77f84dab8cb6e
khurdt <joshuahurdt@gmail.com>
2022-12-10 09:14:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -173,18 +173,18 @@ app.post('/password-reset/:email', (req, res) => {
                     sendPasswordReset(req.params.email, hashedString, user._id).then(result => {
                         res.status(200).json(result)
                     }).catch((error) => {
-                        res.status(500).json({ message: 'failed to send email' });
+                        res.status(500).json({ message: 'failed to send email', error: error });
                     });
                 }).catch((error) => {
-                    res.status(500).json({ message: 'failed to create new password reset' });
+                    res.status(500).json({ message: 'failed to create new password reset', error: error });
                 });
             }).catch((error) => {
-                res.status(500).json({ message: 'failed to delete old password reset' });
+                res.status(500).json({ message: 'failed to delete old password reset', error: error });
             })
         }
     }).catch((error) => {
         console.log(error);
-        res.status(500).json({ message: 'failed to send password reset' })
+        res.status(500).json({ message: 'failed to send password reset', error: error })
     })
 });
 
