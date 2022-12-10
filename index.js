81218654f77f8124039bf46582221833d7e2f2f6
khurdt <joshuahurdt@gmail.com>
2022-12-10 12:00:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -328,28 +328,27 @@ app.put('/password-reset', (req, res) => {
 
     PasswordReset.findOne({ userId: req.body.id }).then((user) => {
         if (user) {
-            if (PasswordReset.validateResetString(req.body.resetString)) {
-                let hashedPassword = Users.hashPassword(req.body.password);
-                res.status(200).json({ hashedPassword: hashedPassword })
-                // Users.findOneAndUpdate({ _id: parseInt(req.body.id) },
-                //     {
-                //         $set:
-                //         {
-                //             password: hashedPassword
-                //         }
-                //     },
-                //     { new: true },// This line makes sure that the updated document is returned
-                //     (err, updatedUser) => {
-                //         if (err) {
-                //             console.error(err);
-                //             res.status(500).send({ message: 'failed to update', err: err });
-                //         } else {
-                //             res.status(200).json(updatedUser);
-                //         }
-                //     });
-            } else {
-                res.status(500).json({ message: `reset string is not valid` });
-            }
+            // if (PasswordReset.validateResetString(req.body.resetString)) {
+            let hashedPassword = Users.hashPassword(req.body.password);
+            Users.findOneAndUpdate({ _id: parseInt(req.body.id) },
+                {
+                    $set:
+                    {
+                        password: hashedPassword
+                    }
+                },
+                { new: true },// This line makes sure that the updated document is returned
+                (err, updatedUser) => {
+                    if (err) {
+                        console.error(err);
+                        res.status(500).send({ message: 'failed to update', err: err });
+                    } else {
+                        res.status(200).json(updatedUser);
+                    }
+                });
+            // } else {
+            //     res.status(500).json({ message: `reset string is not valid` });
+            // }
         } else {
             res.status(500).json({ message: `did not find correct id` });
         }
